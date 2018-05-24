511ea1861da158633b00d9077e174d3b0c3a55f7
areichmann-tgm <areichmann@student.tgm.ac.at>
2018-05-24 15:30:36 +0200

    annotations Reservierung

--- src/main/java/main/main/Reservierung.java
+++ src/main/java/main/main/Reservierung.java
@@ -1,27 +1,46 @@
 package main;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 public class Reservierung {
 
 	@Id
+	@Column
 	private long id;
 
+
+	@NotNull
+	@Column
 	private Date datum;
 
+	@NotNull
+	@Column
 	private int praemienMeilenBonus = 15;
 
+	@NotNull
+	@Column
 	private int preis = 150;
 
+	@NotNull
+	@Column
 	private StatusInfo status;
 
+	@NotNull
+	@Column
 	private Zug zug;
 
+	@NotNull
+	@Column
 	private Strecke strecke;
 
+	@NotNull
+	@Column
 	private Benutzer benutzer;
 
+	@NotNull
+	@Column
 	private Zahlung zahlung;
 
 }
