b47aa330f32832b66dd502980589359fbcf771b2
areichmann-tgm <areichmann@student.tgm.ac.at>
2018-05-24 16:16:58 +0200

    more annotations

--- src/main/java/main/main/Zug.java
+++ src/main/java/main/main/Zug.java
@@ -1,22 +1,36 @@
 package main.main;
 
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
-
+@Entity
 public class Zug {
 
-	private Long ID;
+	@Id
+	@Column
+	private long id;
 
+	@Column
 	private Date startZeit;
 
+	@Column
 	private int sitzPlaetze = 500;
 
+	@Column
 	private int fahrradStellplaetze = 50;
 
+	@Column
 	private int rollStuhlPlaetze = 10;
 
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="start")
 	private Bahnhof start;
 
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="ende")
 	private Bahnhof ende;
 
 }
